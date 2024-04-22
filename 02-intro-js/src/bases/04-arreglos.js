e9dfa539f3158106d182aba433714ec55839057f
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-04-22 13:55:39 -0600

    feat(): arreglos
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

---
+++ 02-intro-js/src/bases/04-arreglos.js
@@ -0,0 +1,24 @@
+// Arreglos
+// const arreglo = new Array( 100 );
+
+const arreglo = [1, 2, 3, 4, 5];
+// arreglo.push(1);
+// arreglo.push(2);
+// arreglo.push(3);
+// arreglo.push(4);
+
+// Operador spread ...
+
+let arreglo2 = [...arreglo, 5];
+// arreglo.push( 5 );
+
+// Call back
+const arreglo3 = arreglo2.map(function (numero) {
+  return numero * 2;
+});
+
+console.log(arreglo);
+console.log(arreglo2);
+console.log(arreglo3);
+
+// Cuando una funcion no tiene un return explicito, esta retornara un undefined
