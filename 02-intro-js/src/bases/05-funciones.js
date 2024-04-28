cfc25da7e97cf8567ea123b77fa26a2f98363ca3
Alejandro <ihernandezramirez800@gmail.com>
2024-04-27 22:28:35 -0600

    feat(js): funciones
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

--- 02-intro-js/src/bases/05-funciones.js
+++ 02-intro-js/src/bases/05-funciones.js
@@ -1,18 +1,40 @@
-// Funciones
+//----------------
+// Funciones en JS
+//----------------
 
+// Funcion comun (No recomendada)
+//-------------------------------
 const saludar = function (nombre) {
   return `Hola, ${nombre}`;
 };
+
+// Usando funcion comun saludar
 console.log(saludar("Nombre 1"));
 
+//Funciones de flecha =>
+//----------------------
 const saludar2 = (nombre) => {
   return `Hola, ${nombre}`;
 };
+
+// Usando funcion de flecha
 console.log(saludar2("Nombre 2"));
 
+// Misma funcion de flecha pero mas ligera
+//----------------------------------------
 const saludar3 = (nombre) => `Hola, ${nombre}`;
+
+// Usando funcion ligera
 console.log(saludar3("Nombre 3"));
 
+// Funcion aun mas resumida
+//-------------------------
+const saludar4 = () => "Hola devs";
+
+// Usando funcion resumida
+console.log(saludar4());
+
+// Funcion de flecha que no recibe ningun argumento
 const getUser = () => ({
   uid: "4545GHJHJ",
   username: "ElUser",
@@ -22,7 +44,9 @@ const user = getUser();
 console.log(user);
 
 // Tarea
+// Esto es una funcion de flecha
 // Esto es un objeto implicito
+// Prueba de la funcion con un console.log
 const getUsuarioActivo = (nombre) => ({
   uid: "G45HJKHH",
   username: nombre,
