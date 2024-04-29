7cf633899684dd25f3a5b45ba6a82016f387b2fc
Alejandro <ihernandezramirez800@gmail.com>
2024-04-29 16:35:26 -0600

    feat(): fetch
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

--- 02-intro-js/src/index.js
+++ 02-intro-js/src/index.js
@@ -1,1 +1,11 @@
-//
+const peticion = "https://rickandmortyapi.com/api/character/";
+
+fetch(peticion)
+  .then((resp) => resp.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+ 
