aacf8d577160c1a641bdb4b68b1332f8a1c1d78e
Alejandro <ihernandezramirez800@gmail.com>
2024-04-30 15:46:44 -0600

    feat(): ternario
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

---
+++ 02-intro-js/src/bases/12-ternario.js
@@ -0,0 +1,18 @@
+// Operador condicional ternario
+const activo = false;
+
+// let mensaje = "";
+
+// if (activo) {
+//   mensaje = "Activo";
+//   console.log(mensaje);
+// } else {
+//   mensaje = "Inactivo";
+//   console.log(mensaje);
+// }
+
+// const mensaje = activo ? "Activo" : "Inactivo";
+
+const mensaje = !activo && "Activo";
+
+console.log(mensaje);
