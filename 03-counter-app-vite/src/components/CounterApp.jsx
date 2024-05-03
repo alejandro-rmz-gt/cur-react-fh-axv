e1152d88406f3102246bf7f62688f4259126deb5
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-05-03 16:42:38 -0600

    feat(): review of useState
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

--- 03-counter-app-vite/src/components/CounterApp.jsx
+++ 03-counter-app-vite/src/components/CounterApp.jsx
@@ -1,18 +1,40 @@
-import React from 'react';
-import PropTypes from "prop-types";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-export const CounterApp = ({ contador }) => {
+export const CounterApp = () => {
+
+    {/* Estado del valor */ }
+    const [contador, setContador] = useState(0);
+
+    {/* Funcion que se manda a llamar con en boton para aumentar */ }
+    const handleAdd = () => {
+        setContador(contador + 1);
+    };
+
+    {/* Funcion que se manda a llamar con el boton para recetear */ }
+    const handleRecet = () => {
+        setContador(0)
+    }
+
+    {/* Retorno del componente */ }
     return (
-        <>
-            <div className='container'>
-                <h2>CounterApp</h2>
-                <strong> {contador} </strong>
-                <button> + 1</button>
+
+        <div className='container'>
+
+            <h2>CounterApp</h2>
+
+            <strong>{contador}</strong>
+
+            <div className='containerButtons'>
+                <button onClick={handleAdd}>Aument</button>
+                <button onClick={handleRecet}> Recet </button>
             </div>
-        </>
-    )
-}
+
+        </div>
+
+    );
+};
 
 CounterApp.propTypes = {
-    contador: PropTypes.number.isRequired,
-}
+    contadorInicial: PropTypes.number,
+};
