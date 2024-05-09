a0d61475fe3d0643bf493adcac11a38dddb160ed
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-05-09 10:17:10 -0600

    feat(): styles and first component

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -2,7 +2,14 @@
 export const GifExpertApp = () => {
     return (
         <>
+            {/* Titulo */}
             <h1>Gif expert app</h1>
+
+            {/* Input */}
+
+            {/* Listado de Gifs */}
+
+                {/* Gif Item */}
         </>
     )
 }
