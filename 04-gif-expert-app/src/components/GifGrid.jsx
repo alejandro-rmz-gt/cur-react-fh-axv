91aebe4df3ac2361f64bf3a7ba1e9e2a7dce7cf4
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-05-12 11:50:46 -0600

    feat(): displaying image titles

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,22 +1,30 @@
-import { useEffect, useState } from "react"; // Importa las funciones useEffect y useState desde React
-import { getGifs } from "../helpers/getGifs"; // Importa la función getGifs desde su archivo correspondiente
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs";
 
-// Define el componente GifGrid
 export const GifGrid = ({ categoria }) => {
-    // Declara el estado para el contador
-    const [counter, setCounter] = useState(10);
 
-    // Efecto secundario que se ejecuta cuando cambia la categoría
+    const [images, setImages] = useState([]);
+
+    const getImages = async () => {
+        const newImages = await getGifs(categoria);
+        setImages(newImages);
+    }
+
     useEffect(() => {
-        // Llama a la función getGifs para obtener los GIFs de la categoría actual
-        getGifs(categoria);
-    }, [categoria]); // Se ejecuta el efecto cada vez que cambia la categoría
+        getImages();
+    }, []);
 
     return (
-        <> {/* Retorna un fragmento que contiene elementos JSX */}
-            <h3>{categoria}</h3> {/* Título de la categoría */}
-            <h5>{counter}</h5> {/* Valor del contador */}
-            <button onClick={() => setCounter(counter + 1)}>+1</button> {/* Botón para incrementar el contador */}
+        <>
+            <h3>{categoria}</h3>
+
+            <ol>
+                {
+                    images.map(({id, title}) => (
+                        <li key={id}>{title}</li>
+                    ))
+                }
+            </ol>
         </>
     );
 }
