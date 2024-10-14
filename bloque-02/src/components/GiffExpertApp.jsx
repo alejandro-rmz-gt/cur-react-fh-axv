4cf3bbdf966783aedd7d825e46a149140e434025
axxving <axxving@gmail.com>
2024-10-14 08:19:29 -0600

    feat: adding styles and iterating categories
    Co-Authored-By: axxving <160192346+axxving@users.noreply.github.com>

--- bloque-02/src/components/GiffExpertApp.jsx
+++ bloque-02/src/components/GiffExpertApp.jsx
@@ -1,4 +1,23 @@
+import { useState } from "react";
 
 export const GiffExpertApp = () => {
-  return <div>GiffExpertApp</div>;
+  // Use state para las categorias
+  const [categories, setCategories] = useState(["One punch man", "MHA"]);
+
+  return (
+    <>
+      {/* Titulo */}
+      <h1>Giph Expert App</h1>
+
+      {/* Input */}
+
+      {/* Listado de items (giphs) */}
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
+      {/* Giph Item */}
+    </>
+  );
 };
