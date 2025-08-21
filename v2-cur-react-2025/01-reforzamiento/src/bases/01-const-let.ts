5b422a6ea2f10b640c9e0d8a7c5d25ebd628a08d
alejandro <axxving@gmail.com>
2025-08-21 12:12:32 -0600

    Variables y constantes

---
+++ v2-cur-react-2025/01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,9 @@
+
+
+const firsName: string = 'Alejandro';
+const lastName: string = 'Hernandez';
+
+const containsLetterH = lastName.includes('H');
+
+console.log({containsLetterH})
+console.log({firsName, lastName});
