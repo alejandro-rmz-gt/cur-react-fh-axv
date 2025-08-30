e78fb65264bbec69b67681db73374eaa54c7913a
alejandro <axxving@gmail.com>
2025-08-29 22:45:42 -0600

    Template strings

---
+++ v2-cur-react-2025/01-reforzamiento/src/bases/02-template-string.ts
@@ -0,0 +1,9 @@
+const firstName = 'Alejandro';
+// caracteres especiales
+const lastName = 'O\' Hernandez'; 
+// Concatenacion con backticks
+const fullname = `El nombre es: ${firstName} ${lastName}`;
+
+// imprimir en consola
+console.log(fullname);
+
