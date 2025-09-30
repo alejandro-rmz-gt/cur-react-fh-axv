9e100140e12754cfd7a1bbbedc74fbc3897f767e
alejandro <axxving@gmail.com>
2025-09-30 09:17:40 -0600

    types y enums

--- v2-cur-react-2025/01-reforzamiento/src/main.ts
+++ v2-cur-react-2025/01-reforzamiento/src/main.ts
@@ -6,7 +6,8 @@ import "./style.css";
 // import './bases/05-arrays';
 // import './bases/06-funciones';
 // import "./bases/07-obj-destructuring";
-import "./bases/08-array-destructuring";
+// import "./bases/08-array-destructuring";
+import "./bases/data/heroes.data";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
